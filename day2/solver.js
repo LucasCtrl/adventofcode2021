ab90757f1ab96026070dd5f1b00a1bddf9da4957
LucasAlt <lucasalt@protonmail.com>
2021-12-02 11:15:17 +0100

    ✨ Complete day 2 - Part 2

--- day2/solver.js
+++ day2/solver.js
@@ -1,15 +1,20 @@
 const fs = require('fs')
-const read = fs.readFileSync('./day2/example.txt')
+const read = fs.readFileSync('./day2/input.txt')
 let data = read.toString().split('\n').map(String)
 
+let aim = 0
 let hPos = 0
 let depth = 0
 
 data.forEach((v) => {
   let instruction = v.split(' ')
-  if (instruction[0] == 'forward') hPos += parseInt(instruction[1])
-  if (instruction[0] == 'up') depth -= parseInt(instruction[1])
-  if (instruction[0] == 'down') depth += parseInt(instruction[1])
+
+  if (instruction[0] == 'down') aim += parseInt(instruction[1])
+  if (instruction[0] == 'up') aim -= parseInt(instruction[1])
+  if (instruction[0] == 'forward') {
+    hPos += parseInt(instruction[1])
+    depth += aim * parseInt(instruction[1])
+  }
 })
 
 console.log(`
